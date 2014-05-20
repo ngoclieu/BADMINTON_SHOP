f06cb6de014ee51d9329952bfa70abb24cc1b7ec
ngoclieu <lieuhn93@gmail.com>
2014-05-20 17:08:53 +0700

    upload

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:89/BADMINTON_SHOP/admin/');
+define('HTTP_CATALOG', 'http://localhost:89/BADMINTON_SHOP/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:89/BADMINTON_SHOP/admin/');
+define('HTTPS_CATALOG', 'http://localhost:89/BADMINTON_SHOP/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\BADMINTON_SHOP/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\BADMINTON_SHOP/system/');
+define('DIR_DATABASE', 'C:\wamp\www\BADMINTON_SHOP/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\BADMINTON_SHOP/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\BADMINTON_SHOP/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\BADMINTON_SHOP/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\BADMINTON_SHOP/image/');
+define('DIR_CACHE', 'C:\wamp\www\BADMINTON_SHOP/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\BADMINTON_SHOP/download/');
+define('DIR_LOGS', 'C:\wamp\www\BADMINTON_SHOP/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\BADMINTON_SHOP/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'BADMINTON');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:89/BADMINTON_SHOP/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:89/BADMINTON_SHOP/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\BADMINTON_SHOP/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\BADMINTON_SHOP/system/');
+define('DIR_DATABASE', 'C:\wamp\www\BADMINTON_SHOP/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\BADMINTON_SHOP/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\BADMINTON_SHOP/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\BADMINTON_SHOP/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\BADMINTON_SHOP/image/');
+define('DIR_CACHE', 'C:\wamp\www\BADMINTON_SHOP/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\BADMINTON_SHOP/download/');
+define('DIR_LOGS', 'C:\wamp\www\BADMINTON_SHOP/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'BADMINTON');
+define('DB_PREFIX', 'oc_');
+?>
